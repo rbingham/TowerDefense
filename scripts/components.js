7267ebd5cdb35add2f50e8b7a741f188645c3c9b
BryantN <BryantN>
2016-03-23 12:50:12 -0600

    Begining add of the game model

---
+++ scripts/components.js
@@ -0,0 +1,13 @@
+/*
+This File holds the structures used to emulate
+the game play, such as enimies, towers
+
+It should be possible to add objects in other files with 
+MyGame.Components.Paddle=(function(){}());
+For
+*/
+MyGame.Components=(function(){
+    var that={};
+    
+    return that;
+}());
