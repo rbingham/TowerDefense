7a09a3b0ba2971d6897963bc3ba38a7364783b3c
Patrick Sharp <psharp.x13@gmail.com>
2016-03-29 17:10:49 -0600

    Added CanvasButton to uiComponents.js

---
+++ scripts/uiComponents.js
@@ -0,0 +1,104 @@
+MyGame.uiComponents = (function(graphics){
+    //ButtonListener: onClick()
+
+    function ButtonListenerMap(){
+        var that={};
+        var listenerMap={};
+        that.addListener = function(key, listener){
+            listenerMap[key]=listener;
+        }
+        that.removeListener = function(key){
+            delete listenerMap[key];
+        }
+        that.onClick = function(evt){
+            for (var key in listenerMap) {
+                  listenerMap[key].onClick(evt);
+            }
+        }
+
+        return that;
+    }
+
+    function CanvasButton(spec){
+        //spec: location{x,y}, drawable, mouseOverDrawable, mouseDownDrawable, redrawQueue
+        var that={};
+        var buttonListenerMap = ButtonListenerMap();
+        var mouseOver=false;
+        var mouseDown=false;
+        var drawable = spec.drawable;
+        var needsRedrawn = true;
+
+        function fireNeedsRedrawn(){
+            needsRedrawn = true;
+            if (spec.hasOwnProperty("redrawQueue")) {
+                spec.redrawQueue.push(that);
+            }
+        }
+
+        that.addButtonListener = function(key, listener){
+            buttonListenerMap.add(key, listener);
+        }
+        that.removeButtonListener = function(key){
+            buttonListenerMap.remove(key);
+        }
+
+        that.onMouseOver = function(){
+            mouseOver = true;
+            if (spec.hasOwnProperty("mouseOverDrawable")) {
+                fireNeedsRedrawn();
+            }
+        }
+
+        that.onMouseAway = function(){
+            var oldMouseDown = mouseDown;
+            mouseDown=false;
+            mouseOver = false;
+            if ((!oldMouseDown && spec.hasOwnProperty("mouseOverDrawable"))
+                    || (oldMouseDown && (spec.hasOwnProperty("mouseDownDrawable")||spec.hasOwnProperty("mouseOverDrawable")))
+            ) {
+                fireNeedsRedrawn();
+            }
+            mouseDown = false;
+        }
+
+        that.onMouseDown(){
+            mouseDown = true;
+            if (spec.hasOwnProperty("mouseDownDrawable")) {
+                fireNeedsRedrawn();
+            }
+        }
+
+        that.onMouseUp(){
+            mouseDown = false
+            if (spec.hasOwnProperty("mouseDownDrawable")) {
+                fireNeedsRedrawn();
+            }
+            buttonListenerMap.onClick();
+        }
+
+        that.draw(){
+            var drawn=false;
+
+            if(mouseOver){
+                if(mouseDown && spec.hasOwnProperty("mouseDownDrawable")){
+                    spec.mouseDownDrawable.draw();
+                    drawn=true;
+                }else if(spec.hasOwnProperty("mouseOverDrawable")){
+                    spec.mouseOverDrawable.draw();
+                    drawn=true;
+                }
+            }
+
+            if(!drawn){
+                drawable.draw();
+            }
+            needsRedrawn = false;
+        }
+
+        return that;
+    }
+
+    return {
+        CanvasButton:CanvasButton
+    }
+}(MyGame.graphics));
