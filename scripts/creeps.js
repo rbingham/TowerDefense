763030a622f1656dbf6647a1f74ffc60972e6e54
Patrick Sharp <psharp.x13@gmail.com>
2016-04-06 17:49:22 -0600

    Added creeps

---
+++ scripts/creeps.js
@@ -0,0 +1,25 @@
+MyGame.components.creeps = (function(){
+	var Creep = function(){
+		//needs to know what it looks like
+		//needs to know where it is
+		//needs to know where it needs to go
+			//needs to know how to get there
+
+		function update(elapsedTime){
+			//update position
+		}
+
+		function draw(elapsedTime){
+			//update sprite
+			//call draw on sprite
+		}
+
+		return {
+
+		};
+	}
+
+	return {
+		Creep:Creep
+	};
+}(););
