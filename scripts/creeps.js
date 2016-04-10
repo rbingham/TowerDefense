4f87dbb70d53ab837921ec899c89a83460343f8d
Patrick Sharp <psharp.x13@gmail.com>
2016-04-10 12:39:04 -0600

    Worked on shortest path and creeps

--- scripts/creeps.js
+++ scripts/creeps.js
@@ -4,6 +4,55 @@
 		shortest path algorithm
 **********************************************************/
 MyGame.components.creeps = (function(){
+	"use strict"
+	/**********************************************************
+		The Creep Manager
+			Knows about all the creeps
+			Creates creeps
+
+		spec:{arena, initialLocations[][], goals[][]}
+	**********************************************************/
+	var CreepManager = function(spec){
+		var that = {};
+		var nextCreepId = 0;
+		var creeps = {};
+
+		var shortestPaths = (function(){
+			var sp = [];
+			for(let i=0; i<spec.endGoals.length; i++){
+				sp[i] = SortestPath({arena:arena, goals:goals[i]});
+			}
+			return sp;
+		}());
+
+		//maybe keep track of creeps by endGoal
+
+		/**********************************************************
+			Creep Creator
+			spec:{creepSpec[], counts[], intervals[]}
+		**********************************************************/
+		that.create = function(spec){
+			spec.id = nextCreepId
+		}
+
+		that.testPotentialArena(){
+
+		}
+
+		//could also just reset shortests paths if arena is not a new instance
+		that.setCurrentArena(){
+
+		}
+
+		that.update(elapsedTime){
+
+		}
+
+		that.render
+
+		return that;
+	}
+
 
 	/**********************************************************
 		The Creep
@@ -12,27 +61,67 @@ MyGame.components.creeps = (function(){
 			knows where it needs to go
 				knows how to find out how to get there
 
+			the difference between air and land creep is their shortest path
+
 			takes a creepListener which is just any object that has the functions:
 				creepKilled(id)
 				creepReachedGoal(id)
 
-		spec:{id, canvas, drawable, initialLocation, shortestPath, initialHP, creepListener}
+		spec:{id, arena, drawable, initialLocation, initialHP, creepSpeed, shortestPath, creepListener}
 	**********************************************************/
 	var Creep = function(spec){
+		var that = {};
+
 		var hp = spec.initialHP;
 
-		function hit(amount){
-			//update hp
-			//if dead call creepListener.creepKilled(spec.id);
+		var shortestPath = spec.shortestPath;
+		var currentGoal = {};
+		var currentLocation = spec.initialLocation;
+
+		that.getHP(){
+			return hp;
 		}
+
+		that.hit = function(amount){
+			hp-=amount;
+			if(hp<=0) creepListener.creepKilled(that);
+		}
+
+		that.isShortestPathValid = function(potentialShortestPath){
+			var distanceFromGoal = that.getDistanceFromGoal(potentialShortestPath);
+			return typeof distanceFromGoal !== undefined;
+		}
+
+		that.setShortestPath = function(newShortestPath){
+			shortestPath = newShortestPath;
+		}
+
+		/**********************************************************
+			getDistanceFromEndGoal()
+			uses shortestPath to find out distance from goal
+			if return value is undefined than path is obstructed
+		**********************************************************/
+		that.getDistanceFromEndGoal = function(potentialShortestPath){
+			var shortestPathToTest;
+			if(potentialShortestPath){
+				shortestPathToTest = potentialShortestPath;
+			}else{
+				shortestPathToTest = spec.shortestPath;
+			}
+
+			//get current rounded x,y
+			//get distance from rounded currentXY
+		}
+
+
 
 		/**********************************************************
 		* update creep
 		**********************************************************/
-		function update(elapsedTime){
+		that.update = function(elapsedTime){
 			//while there is elapsedTime left
-				//given current location get next goal from shortestPath
-				//given next goal move as close to it as elapsedTime will allow
+				//given current location and next goal
+					//move as close to it as elapsedTime will allow
 				//update current location
 				//decrement the elapsedTime used
 
@@ -44,14 +133,12 @@ MyGame.components.creeps = (function(){
 		/**********************************************************
 		* render creep
 		**********************************************************/
-		function draw(elapsedTime){
-			//update sprite
+		that.draw = function(elapsedTime){
+			//update sprite?
 			//call draw on sprite
 		}
 
-		return {
-
-		};
+		return that;
 	}
 
 	/**********************************************************
@@ -62,45 +149,76 @@ MyGame.components.creeps = (function(){
 		spec:{arena, goals}
 	**********************************************************/
 	var ShortestPath = function(spec){
-		var goalMatrix = [];
+		//[][] {location,distance}
+		var distanceFromEndGoalMatrix = (function(){
+			var i;
+
+
+			var matrix = [];
+			for(i=0; i<spec.arena.rowCount; i++){
+				arena[i]=[];
+			}
+
+			var endIndex=0;
+			var workQueue = [];
+			for(i=0; i<spec.goals.length; i++){
+				workQueue.push({location:spec.goals[i], distance:0});
+				endIndex++;
+			}
+
+			var work;
+			var x,y;
+			var nextDistance;
+
+			for(i=0; i<endIndex; i++){
+				work = workQueue[i];
+				x=work.location.x;
+				y=work.location.y;
+				if(
+					//arena @ x,y is valid and not occupied
+					&& (
+						typeof matrix[x][y] === undefined
+						|| work.distance < matrix[x][y].distance
+					)
+				){
+
+					matrix[x][y] = work;
+
+					nextDistance = work.distance+1;
+					workQueue.push({location:{x:x+1,y:y}}, distance:nextDistance});
+					workQueue.push({location:{x:x-1,y:y}}, distance:nextDistance});
+					workQueue.push({location:{x:x,y:y+1}}, distance:nextDistance});
+					workQueue.push({location:{x:x,y:y-1}}, distance:nextDistance});
+
+					nextDistance = work.distance+Math.sqrt(2);
+					workQueue.push({location:{x:x+1,y:y+1}}, distance:nextDistance});
+					workQueue.push({location:{x:x-1,y:y+1}}, distance:nextDistance});
+					workQueue.push({location:{x:x+1,y:y-1}}, distance:nextDistance});
+					workQueue.push({location:{x:x-1,y:y-1}}, distance:nextDistance});
+
+					endIndex+=8;
+				}
+				delete workQueue[i];
+			}
+
+			return matrix;
+		}());
 
 		/**********************************************************
 			given a location return the location of an unobstructed goal
 		**********************************************************/
-		function getGoal(location){
-			//lookup current location in a map or 2d array
-			if (
-				a[location.x] !== undefined
-				&& a[location.x][location.y] !== undefined
-			){
-				//if goal exists return goal
-				return a[location.x][location.y];
-			}else{
-				//else return calculate goal
-				return calculateGoal(location);
-			}
+		function getNextGoals(location){
+			//lookup all adjacent and diagonal location in the matrix
+			//return all locations that have the lowest distance from endGoal
 		}
 
-		function clear(){
-			goalMatrix = [];
-		}
-
-		function calculateGoal(location){
-			//remember to take into account all goals
-
-			//a* would be best but the amount of code is pretty heavy
-
-			//maybe breadth first? (this would be the easiest to code)
-				//maybe after breadth first optimize to find strait paths
-
-			//durring breadth first take into account previously found paths
-
-			//after a path or unobstructed goal is found
-				//update all applicable matrix locations to reflect goal
+		function getDistanceFromGoal(location){
+			return distanceFromEndGoalMatrix[location.x][location.y];
 		}
 
 		return {
-
+			getNextGoals:getNextGoals,
+			getDistanceFromGoal:getDistanceFromGoal;
 		};
 	}
 
